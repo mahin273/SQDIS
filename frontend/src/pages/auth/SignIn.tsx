960618fb77c60af7a1302df00913791e48447ff6
ZamanAsad007 <asadasif1704@gmail.com>
2026-04-26 17:19:48 +0600

    fixed file missmatch

--- frontend/src/pages/auth/SignIn.tsx
+++ frontend/src/pages/auth/SignIn.tsx
@@ -66,7 +66,7 @@ export default function SignIn() {
             type="button"
             className="w-full rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-white hover:bg-black"
           >
-            Login
+            Sign-in
           </button>
         </form>
 
